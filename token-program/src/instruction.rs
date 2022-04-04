ab31b85e0e25f6740a7205276ce31480bdf22c0b
Omid Ashtari <oashtari@gmail.com>
2022-04-04 09:03:29 -0700

    initial commit for chainlink solana bootcamp

---
+++ token-program/src/instruction.rs
@@ -0,0 +1,10 @@
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
+pub enum TokenInstruction {
+   CreateToken,
+   CreateTokenAccount,
+   Mint { amount: u64 },
+   Transfer { amount: u64 },
+}
---
+++ token-program/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod entrypoint;
+pub mod instruction;
+pub mod processor;
+pub mod state;
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
